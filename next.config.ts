c35559a03db07df2d1e99e379791c466a333b496
sonu-shivcharan <sonushivcharan@outlook.com>
2026-09-19 16:44:24 +0530

    feat: update environment configuration and add AWS SDK dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-    allowedDevOrigins: ['127.0.0.1'],
+  allowedDevOrigins: ["127.0.0.1", "10.20.24.33"],
 }
 
 export default nextConfig
